887f6bee58a5fe3f421de2b7ef87d4abe1c1958b
Messias Dias <messiasdias.ti@gmail.com>
2025-02-06 20:35:04 -0300

    Atualiza Readme e pagina popup.html

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bafo
+### Block Abas From Origins
+
+Uma extensão simples para o Google Chrome, que fechas automaticamente novas abas contendo páginas de anúcios abertas e originadas apartir dos sites/domínios listados.
+
+#### Funcionalidades Existentes
+* Lista sites/domínios pré adicionados por padrão na extenção e destaca os sites reconhecidos;
+
+#### Funcionalidades Futuras
+* Adicionar novos sites/domínios manualmente
+* Editar novos sites/domínios adicionados
+* Remover novos sites/domínios adicionados
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", async function () {
     const sites = await fetch('sites.json').then(v => v.json());
     const list = document.getElementById("tab-list");
     list.innerHTML = "";
+
     chrome.tabs.query({}, (tabs) => {
         sites?.forEach((site) => {
-            let tabActive = tabs.find(tab => tab.url.includes(site))
             let li = document.createElement("li");
-            if(tabActive){li.classList.add('active');}
             li.textContent = `${site}`;
+            (tabs.find(tab => tab.url.includes(site)) && li.classList.add('active'))
             list.append(li, document.createElement("br"));
         });
     });
