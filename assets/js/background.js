50249857d65fae0c273e011013a106bd390764de
Messias Dias <messiasdias.ti@gmail.com>
2025-02-14 17:02:31 -0300

    Atualiza sites.json

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,46 +1,82 @@
-// sw.js:
-self.addEventListener('install', async event => {
-    const sites = await fetch('/sites.json').then(v => v.json());
-    
-    chrome.action.openPopup();
-
-    const extractDomainFromUrl = (url) => {
-        var result = ""
-        var match
-        if (match = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)/im)) {
-            result = match[1]
-            if (match = result.match(/^[^\.]+\.(.+\..+)$/)) {
-                result = match[0] || match[1]
-            }
+const extractDomainFromUrl = (url) => {
+    try {
+        let dominio = new URL(url).hostname, partes = dominio.split('.');
+        if (partes.length > 2) {
+            return partes.slice(-2).join('.');
         }
-        return result
+        return dominio;
+    } catch (e) {
+        return null;
     }
+};
 
-    const getSiteTab = (callback = (tab) => tab) => {
-        chrome.tabs.query({}, tabs => {
-            callback(tabs?.filter(tab => sites.includes(extractDomainFromUrl(tab.url))))
-        })
-    }
-
-    chrome.tabs.onCreated.addListener(created => {
-        getSiteTab((tabs) => {
-            tabs?.forEach(tab => {
-                if (created.openerTabId === tab.id) {
-                    console.log("created --> getSiteTab", created.openerTabId === tab.id)
-                    chrome.tabs.remove(created.id)
-                }
-            });
-        });
+const getSiteTab = async (callback = (tab) => tab) => {
+    const sites = await fetch('/sites.json').then(v => v.json());
+    chrome.tabs.query({}, async tabs => {
+        const ftabs = await tabs?.filter(tab => sites.includes(extractDomainFromUrl(tab.url)))
+        if (ftabs[0]?.windowId) {
+            chrome.storage.local.set({ windowId: ftabs[0].windowId })
+        }
+        callback(ftabs)
     });
+};
 
-    chrome.tabs.onActivated.addListener(actived => {
-        getSiteTab((tabs) => {
-            tabs?.forEach(tab => {
-                if (actived.openerTabId === tab.id) {
-                    console.log("actived --> getSiteTab", actived.openerTabId === tab.id)
-                    chrome.tabs.remove(actived.id)
-                }
+const initListeners = async () => {
+    const storage = await chrome.storage.local.get();
+
+    if (storage.loop === false) {
+        chrome.tabs.onCreated.removeListener();
+        chrome.tabs.onActivated.removeListener();
+        chrome.windows.onFocusChanged.removeListener();
+        return console.debug('Removing listeners...');
+    }
+
+    if (storage.loop === true) {
+        chrome.tabs.onCreated.addListener(created => {
+            getSiteTab((tabs) => {
+                tabs?.forEach(tab => {
+                    if (created.openerTabId === tab.id) {
+                        try {
+                            chrome.tabs.remove(created.id);
+                        } catch (e) {
+                            console.warn(e)
+                        }
+                    }
+                });
             });
         });
-    })
+
+        chrome.tabs.onActivated.addListener(actived => {
+            getSiteTab((tabs) => {
+                tabs?.forEach(tab => {
+                    if (actived.openerTabId === tab.id) {
+                        try {
+                            chrome.tabs.remove(actived.id);
+                        } catch (e) {
+                            console.warn(e)
+                        }
+                    }
+                });
+            });
+        });
+
+        chrome.windows.onFocusChanged.addListener(async (windowId) => {
+            const storage = await chrome.storage.local.get();
+            if (
+                windowId > 0 &&
+                windowId != storage.windowId
+            ) {
+                chrome.windows.remove(windowId)
+            }
+        })
+
+        console.debug('Starting listeners...');
+    }
+}
+
+self.addEventListener('install', async event => {
+    console.debug('Installing service-worker...');
+    initListeners();
+    chrome.action.openPopup();
+    chrome.storage.local.onChanged.addListener(initListeners);
 });
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,6 +1,13 @@
+const startStopBtnSetText = async (btn, started = false) => {
+    if (started === true) {
+        return btn.innerHTML = "Parar"
+    }
+    btn.innerHTML = "Iniciar"
+};
+
 document.addEventListener("DOMContentLoaded", async function () {
     const sites = await fetch('sites.json').then(v => v.json());
-    const list = document.getElementById("tab-list");
+    const list = document.querySelector("#tab-list");
     list.innerHTML = "";
 
     chrome.tabs.query({}, (tabs) => {
@@ -11,4 +18,20 @@ document.addEventListener("DOMContentLoaded", async function () {
             list.append(li, document.createElement("br"));
         });
     });
+
+    let storage = await chrome.storage.local.get();
+    const startStopBtn = document.querySelector('#start-stop-btn')
+
+    startStopBtn.addEventListener("click", async () => {
+        storage = await chrome.storage.local.get();
+        await chrome.windows.getCurrent(window => {
+            chrome.storage.local.set({
+                loop: !storage.loop,
+                windowId: window.id
+            });
+            startStopBtnSetText(startStopBtn, !storage.loop);
+        })
+    });
+
+    startStopBtnSetText(startStopBtn, storage.loop);
 });
